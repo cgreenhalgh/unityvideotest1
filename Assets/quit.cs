93ca4d0a4ff7415d595e723b14deb7a31061bad5
Chris Greenhalgh <chris.greenhalgh@nottingham.ac.uk>
2019-07-10 17:20:21 +0100

    initial video version

---
+++ Assets/quit.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class quit : MonoBehaviour
+{
+    public Button yourButton;
+
+    void Start()
+    {
+        Debug.Log("Hello");
+        if (yourButton)
+        {
+            Button btn = yourButton.GetComponent<Button>();
+            btn.onClick.AddListener(OnQuit);
+        }
+    }
+
+    public void SetScene(string scene)
+    {
+        Debug.Log("Load scene " + scene);
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+      
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Debug.Log("escaping...");
+            Application.Quit();
+        }
+
+    }
+
+    public void OnQuit() {
+        Debug.Log("bye!");
+        Application.Quit();
+    }
+
+    public void Test()
+    {
+        Debug.Log("test");
+    }
+}
